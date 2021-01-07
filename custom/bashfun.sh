879b7ae24fc712072fc8e1cda86466486a64acb8
snototter <h.possegger+dev@gmail.com>
2021-01-07 20:21:57 +0100

    use systemd timer

--- custom/bashfun.sh
+++ custom/bashfun.sh
@@ -4,16 +4,16 @@
 
 function rec_resolve_dir
 {
-  # Recursively resolve the given directory. Adapted from https://stackoverflow.com/a/246128/400948
+  # Recursively resolves the given directory. Taken from https://stackoverflow.com/a/246128/400948
   src="$1"
   while [ -h "$src" ]; do # resolve $src until the file is no longer a symlink
-      dname="$( cd -P "$( dirname "$src" )" >/dev/null 2>&1 && pwd )"
-      src="$(readlink "$src")"
+      dname="$( cd -P "$( dirname "${src}" )" >/dev/null 2>&1 && pwd )"
+      src="$(readlink "${src}")"
       # if $src was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-      [[ $src != /* ]] && SOURCE="$dname/$Ssrc"
+      [[ $src != /* ]] && SOURCE="${dname}/${src}"
   done
-  resdir="$(cd -P "$( dirname "$src" )" >/dev/null 2>&1 && pwd)"
-  echo "$resdir"
+  resdir="$(cd -P "$( dirname "${src}" )" >/dev/null 2>&1 && pwd)"
+  echo "${resdir}"
 }
 
 
@@ -30,12 +30,10 @@ function install_service
   svc=$1.service
   sysvc=/etc/systemd/system/$svc
 
-  if [ -f "$sysvc" ]
+  if [ -f "${sysvc}" ]
   then
-    # Stop and disable previously registered instances
-    echo "Need to stop/disable previously registered instance ($sysvc):"
-    systemctl disable $svc
-    systemctl stop $svc
+    echo "Stopping previously registered service unit: ${sysvc}"
+    systemctl stop $svc || true
   fi
   
 
@@ -50,3 +48,37 @@ function install_service
   systemctl enable $svc
   systemctl start $svc
 }
+
+
+function install_timer
+{
+  # Installs the "$1".timer file (must be located within the current directory)
+  # into /etc/systemd/system/ and replaces my custom placeholders 
+  #
+  # Params:
+  # $1 NAME_OF_TIMER (without extension)
+  # $2 TIMERSPECIFICATION
+  timerspec=$2
+
+  svc=$1.timer
+  sysvc=/etc/systemd/system/$svc
+
+  if [ -f "${sysvc}" ]
+  then
+    echo "Stopping previously registered timer unit: ${sysvc}"
+    systemctl stop $svc || true
+  fi
+  
+
+  # Create/replace the service file
+  cp $svc /etc/systemd/system
+  sed -i "s/TIMERSPECIFICATION/$timerspec/g" "$sysvc"
+
+  echo "Registering timer '$svc'"
+
+  # Reload and enable timer
+  systemctl daemon-reload
+  systemctl enable $svc
+  systemctl start $svc
+}
+
--- custom/splash-cycler/01-install.sh
+++ custom/splash-cycler/01-install.sh
@@ -1,7 +1,37 @@
 #!/bin/bash --
 
+set -e
+
 source ../bashfun.sh
 
-scriptdir=$(rec_resolve_dir ${BASH_SOURCE[0]})
-install_service custom-splash-screens "$scriptdir"
+function print_usage
+{
+  echo "Usage: ${BASH_SOURCE[0]} [-t oncalendar]"
+  echo "  Optionally, specify the OnCalendar argument for the timer via -t"
+  echo "  Refer to the date specifications: https://www.freedesktop.org/software/systemd/man/systemd.time.html"
+  echo "  and use 'systemd-analyze calendar \"your-datestring\"' to verify."
+}
 
+# Default recurrence: run at 06:05, 12:05 and 18:05:
+oncal="*-*-* 06,12,18:05:00"
+while getopts 't:' 'OPTKEY'
+do
+  case "${OPTKEY}" in
+    't') oncal=${OPTARG};;
+    *)
+      print_usage
+      exit 1
+      ;;
+  esac
+done
+
+echo "Verifying timer's OnCalendar setting:"
+systemd-analyze calendar "$oncal"
+
+svcunit="custom-splash-screens"
+echo "Installing ${svcunit}.service"
+scriptdir=$(rec_resolve_dir ${BASH_SOURCE[0]})
+install_service "$svcunit" "$scriptdir"
+
+echo "Installing timer"
+install_timer "$svcunit" "$oncal"
